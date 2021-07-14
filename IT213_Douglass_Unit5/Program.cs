f7510843c9eb722b0550573b89e6ad753ccb4c3c
Dax Douglass <dax.douglass@gmail.com>
2021-07-14 11:23:02 -0400

    Add project files.

---
+++ IT213_Douglass_Unit5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace IT213_Douglass_Unit5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence = "The quick brown fox jumps over the lazy dog.";
+            string[] words = sentence.Split(' ');                   // Use string method to split words from the sentence
+            int cnt = 0;
+
+            foreach (var word in words)                             // Get each word from the sentence
+            {
+                cnt++;                                              // Increment counter
+                Console.WriteLine($"{cnt} {word.TrimEnd('.')}");    // Print the position the word appears in the sentence
+                                                                    // along with the word and strip the period from the end
+
+            }
+           
+        }
+    }
+}
